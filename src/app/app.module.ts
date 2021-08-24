fafb88d2e40b86575de2bcd2486d8c764b929ff6
FedeBatt <59941634+FedeBatt@users.noreply.github.com>
2021-08-24 19:59:52 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeroeModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroeModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
